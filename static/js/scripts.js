7291d4d2583df7a02327f730ae1ac44f13821311
Gonzalo Lochocki <gonlochocki@gmail.com>
2023-11-11 12:13:01 -0300

    Falla! io is not defined

--- static/js/scripts.js
+++ static/js/scripts.js
@@ -3,7 +3,7 @@ const socket = io()
 const listProducts = document.querySelector("#products");
 
 socket.on("addProduct", product => {
-    const item = document.createElement.("li");
+    const item = document.createElement("li");
     item.innerHTML = product;
     listProducts.appendChild(item);
 })
