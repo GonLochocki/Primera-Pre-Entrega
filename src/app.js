648b6abbd3262f60b3d69cfce83e1cb41777f8c8
Gonzalo Lochocki <gonlochocki@gmail.com>
2023-11-11 16:20:26 -0300

    Version Actual funcionando

--- src/app.js
+++ src/app.js
@@ -3,6 +3,7 @@ import { productRouter } from "./productRouter.js";
 import { cartRouter } from "./cartRouter.js";
 import { engine } from "express-handlebars";
 import { Server } from "socket.io";
+import { webRouter } from "./webRouter.js";
 
 const app = express();
 app.use(express.json());
@@ -10,8 +11,9 @@ app.engine("handlebars", engine());
 app.set("views", "./views");
 app.set("view engine", "handlebars");
 app.use("/static", express.static("./static"));
-app.use("/api/products", productRouter);
-app.use("/api/cart", cartRouter);
+// app.use("/api/products", productRouter);
+// app.use("/api/cart", cartRouter);
+app.use("/", webRouter);
 
 const server = app.listen(8080, () => {
   console.log("PORT 8080 is conected!");
@@ -20,20 +22,22 @@ const server = app.listen(8080, () => {
 const ioServer = new Server(server);
 
 app.use((req, res, next) => {
-  req[io] = ioServer;
+  req.io = ioServer;
   next();
 });
 
-ioServer.on("connection", (socket) => {
+ioServer.on("connection", (socket) => {  
   socket.on("addProduct", (product) => {
-    ioServer.sockets.emit("addProduct", product);
+    console.log(product);
+    req.io.sockets.emit("addProduct", product);
   });
 });
 
 app.post("/api/products", async (req, res) => {
   const product = req.body;
   try {
-    ioServer.sockets.emit("addProduct", product);
+    req.io.sockets.emit("addProduct", product);
+    res.json(product)
   } catch (error) {
     res.json({ error: error.message });
   }
--- static/js/scripts.js
+++ static/js/scripts.js
@@ -5,5 +5,6 @@ const listProducts = document.querySelector("#products");
 socket.on("addProduct", product => {
     const item = document.createElement("li");
     item.innerHTML = product;
+    item.textContent = JSON.stringify(product)
     listProducts.appendChild(item);
 })
